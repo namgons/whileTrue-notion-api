b9c39d38e271dc3c383e498cdcb802c0457d0d39
Lee, Nam Gon <nglee.dev@gmail.com>
2024-02-07 23:07:34 +0900

    [feat] create page 요청 생성

--- src/app.ts
+++ src/app.ts
@@ -3,20 +3,31 @@ import express, { Application, Request, Response } from "express";
 import startOAuthProcess from "./oauth";
 import { NotionAccessTokenProps } from "./notion/props";
 import { printProblems } from "./notion/utils";
-import { retrieveDatabae } from "./notion/test";
+import { createPage, retrieveDatabae } from "./notion/test";
 
 const app: Application = express();
 
-app.get("/oauth/:code", async (req: Request, res: Response) => {
+app.get("/api/member/oauth/:code", async (req: Request, res: Response) => {
   const code = req.params.code;
   const token: NotionAccessTokenProps = await startOAuthProcess(code);
   res.send({
     response: token,
   });
+
+  console.log("\n\n================================================\n\n");
+  console.log("token :", token);
+  console.log("\n\n================================================\n\n");
 });
 
 app.get("/retrieve-database", async (req: Request, res: Response) => {
   const response = await retrieveDatabae();
+  res.send({
+    response,
+  });
+});
+
+app.get("/create-page", async (req: Request, res: Response) => {
+  const response = await createPage();
   res.send({
     response,
   });
