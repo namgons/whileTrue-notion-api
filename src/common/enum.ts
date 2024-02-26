9d8ed462682067804f542b5317805524e061ef05
Lee, Nam Gon <nglee.dev@gmail.com>
2024-02-26 19:58:50 +0900

    [feat] enum 클래스

--- src/common/enum.ts
+++ src/common/enum.ts
@@ -1,1 +1,25 @@
-export enum RequiredColumnsName {}
+export enum RequiredColumnName {
+  PROBLEM_SITE = "출처",
+  PROBLEM_LEVEL = "난이도",
+  PROBLEM_NUMBER = "문제 번호",
+  PROBLEM_TITLE = "문제 제목",
+  PROBLEM_URL = "URL",
+}
+
+export enum RequiredColumnType {
+  PROBLEM_SITE = "select",
+  PROBLEM_LEVEL = "select",
+  PROBLEM_NUMBER = "number",
+  PROBLEM_TITLE = "title",
+  PROBLEM_URL = "url",
+}
+
+export enum IconType {
+  EMOJI = "emoji",
+  EXTERNAL = "external",
+}
+
+export enum SiteType {
+  BOJ = "백준",
+  PROGRAMMERS = "프로그래머스",
+}
