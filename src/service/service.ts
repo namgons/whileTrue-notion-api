9cb2bfca39b96a8c3a4f74af390fc9640020337e
Lee, Nam Gon <nglee.dev@gmail.com>
2024-07-06 23:59:25 +0900

    [refactor] 함수 분리

--- src/service/service.ts
+++ src/service/service.ts
@@ -1,3 +1,4 @@
+import { error } from "console";
 import { ProblemPage } from "../common/class";
 import { IconType, RequiredColumnName, RESP_STATUS } from "../common/enum";
 import { convertStringToIconType, convertStringToSiteType, isDatabaseValid } from "../common/utils";
@@ -12,37 +13,34 @@ export const checkDatabase = async ({
   notionApiKey,
   databaseId,
 }: DefaultDatabaseRequestDto): Promise<CheckDatabaseResponseDto> => {
-  let response: any;
   try {
-    response = await retrieveDatabase({ notionApiKey, databaseId });
+    const response: any = await retrieveDatabase({ notionApiKey, databaseId });
+
+    if (response.object !== "database") {
+      return new CheckDatabaseResponseDto(RESP_STATUS.NOT_FOUND);
+    }
+
+    const databaseIconType = convertStringToIconType(response?.icon?.type);
+    const databaseIconSrc = getIconSrcByType(databaseIconType, response);
+    const databaseTitle = response.title[0].plain_text;
+
+    if (!isDatabaseValid(response.properties)) {
+      return new CheckDatabaseResponseDto(RESP_STATUS.INVALID);
+    }
+
+    return new CheckDatabaseResponseDto(
+      RESP_STATUS.SUCCESS,
+      response.id,
+      databaseIconType,
+      databaseIconSrc,
+      databaseTitle
+    );
   } catch (error: any) {
     if (error.status === 404) {
       return new CheckDatabaseResponseDto(RESP_STATUS.NOT_FOUND);
     }
+    throw error;
   }
-
-  if (response.object !== "database") {
-    return new CheckDatabaseResponseDto(RESP_STATUS.NOT_FOUND);
-  }
-
-  const newDatabaseId = response.id;
-  const databaseIconType = convertStringToIconType(response?.icon?.type);
-  const databaseIconSrc = getIconSrcByType(databaseIconType, response);
-
-  const databaseTitle = response.title[0].plain_text;
-  const properties = response.properties;
-
-  if (!isDatabaseValid(properties)) {
-    return new CheckDatabaseResponseDto(RESP_STATUS.INVALID);
-  }
-
-  return new CheckDatabaseResponseDto(
-    RESP_STATUS.SUCCESS,
-    newDatabaseId,
-    databaseIconType,
-    databaseIconSrc,
-    databaseTitle
-  );
 };
 
 export const getAllProblemList = async ({
@@ -51,7 +49,6 @@ export const getAllProblemList = async ({
 }: DefaultDatabaseRequestDto): Promise<ProblemListResponseDto> => {
   let hasMore = true;
   let nextCursor: any = undefined;
-
   const problemPageList = [];
 
   while (hasMore) {
@@ -63,25 +60,10 @@ export const getAllProblemList = async ({
     hasMore = response.has_more;
     nextCursor = response.next_cursor;
 
-    for (let page of response.results) {
-      const iconType = convertStringToIconType(page?.icon?.type);
-      const iconSrc = getIconSrcByType(iconType, page);
-
-      const properties = page.properties;
-
-      if (!isDatabaseValid(properties)) {
-        return new ProblemListResponseDto(RESP_STATUS.INVALID);
-      }
-
-      const { siteType, level, number, titleList, url } = parseProperty(properties);
-
-      if (!isColumnDataIncluded(siteType, level, number, titleList, url)) {
-        continue;
-      }
-
-      problemPageList.push(
-        new ProblemPage(siteType, level, number, titleList[0].plain_text, url, iconType, iconSrc)
-      );
+    try {
+      problemPageList.push(...parseProblemPages(response.results));
+    } catch {
+      return new ProblemListResponseDto(RESP_STATUS.INVALID);
     }
   }
 
@@ -101,40 +83,67 @@ export const saveNewProblem = async ({
   }
 };
 
-const parseProperty = (properties: any) => {
-  return {
-    siteType: convertStringToSiteType(properties[RequiredColumnName.PROBLEM_SITE].select?.name),
-    level: properties[RequiredColumnName.PROBLEM_LEVEL].select?.name,
-    number: properties[RequiredColumnName.PROBLEM_NUMBER]?.number,
-    titleList: properties[RequiredColumnName.PROBLEM_TITLE]?.title,
-    url: properties[RequiredColumnName.PROBLEM_URL]?.url,
-  };
+const parseProblemPages = (results: any): Array<ProblemPage> => {
+  const problemPageList = [];
+
+  for (const page of results) {
+    const properties = page.properties;
+
+    if (!isDatabaseValid(properties)) {
+      throw error;
+    }
+
+    const problemData = parseProperty(properties);
+
+    if (isValidProblemData(problemData)) {
+      const iconType = convertStringToIconType(page?.icon?.type);
+      const iconSrc = getIconSrcByType(iconType, page);
+
+      problemPageList.push(
+        new ProblemPage(
+          problemData.siteType,
+          problemData.level,
+          problemData.number,
+          problemData.titleList[0].plain_text,
+          problemData.url,
+          iconType,
+          iconSrc
+        )
+      );
+    }
+  }
+  return problemPageList;
 };
+
+const parseProperty = (properties: any) => ({
+  siteType: convertStringToSiteType(properties[RequiredColumnName.PROBLEM_SITE].select?.name),
+  level: properties[RequiredColumnName.PROBLEM_LEVEL].select?.name,
+  number: properties[RequiredColumnName.PROBLEM_NUMBER]?.number,
+  titleList: properties[RequiredColumnName.PROBLEM_TITLE]?.title,
+  url: properties[RequiredColumnName.PROBLEM_URL]?.url,
+});
 
 const getIconSrcByType = (iconType: IconType | null, data: any) => {
   switch (iconType) {
     case IconType.EMOJI:
       return data.icon.emoji;
-
     case IconType.EXTERNAL:
       return data.icon.external.url;
-
     case IconType.FILE:
       return data.icon.file.url;
-
     default:
       return null;
   }
 };
 
-const isColumnDataIncluded = (siteType: any, level: any, number: any, titleList: any, url: any) => {
-  return !(
-    siteType === null ||
-    level === null ||
-    number === null ||
-    titleList === null ||
-    titleList.length === 0 ||
-    titleList[0]?.plain_text === null ||
-    url === null
+const isValidProblemData = ({ siteType, level, number, titleList, url }: any) => {
+  return (
+    siteType !== null &&
+    level !== null &&
+    number !== null &&
+    titleList !== null &&
+    titleList.length > 0 &&
+    titleList[0]?.plain_text !== null &&
+    url !== null
   );
 };
